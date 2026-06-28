2c866b11c75184ed2dafdc40836f0a26aecae21b
Firza NH <firzan6@gmail.com>
2026-06-29 00:11:13 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Pastikan mengarah ke file App Anda
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
